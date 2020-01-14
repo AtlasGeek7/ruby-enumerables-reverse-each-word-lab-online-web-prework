854ac442c3d85c9968bf42d15e6344aaef633b71
Khalid Tissire <khalid.tissire@gmail.com>
2020-01-14 12:36:37 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def  reverse_each_word(str)
+  word = ""
+  str.split(' ').each.with_index do |s,index|
+    index < str.split(' ').length-1 ? delimiter = " " : delimiter = ""
+    word += s.split('').reverse().join('') + delimiter
+  end
+  return word
+end
